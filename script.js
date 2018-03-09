b03137e809e54ead9f035325202390ab6d13516a
rob-now <rnowakowski.prv@gmail.com>
2018-03-09 12:58:18 +0100

    For now communicating with user only by confirm and alert messages + added comments

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 var ready = confirm("Are you ready to play?");
 var youDie = "\n\nYOU DIED!";
 
+// Ready to play?
 if (ready === false) {
-    console.log("You choose not to play.");
+    confirm("You choose not to play.");
 }
 else {
+    // Asking for age
     var age = parseInt(prompt("What is your age?"));
-
-    if (age < 15) {
+    //
+    if (!age) {
+        alert("Sorry, you cannot play. You didn't enter your age.");
+    }
+    // Too young
+    else if (age < 15) {
         alert("Sorry, you cannot play. You are under 15 years old.");
     }
+    // Can play
     else {
-        console.log("You wake up in some kind of cave wondering what happened. After a while you hear a huge roar coming from the dark. The time for thinking of past is done as you here some movement far from your back. Now you need to run. But where is the exit...?");
-        console.log("\nYou take first steps. You see stairs. You can go upstairs or downstairs.");
-
+        // Waking up
+        alert("You wake up in some kind of cave wondering what happened. After a while you hear a huge roar coming from the dark. The time for thinking is done as you hear some movement far from your back. Now you need to run. But where is the exit...?\n\nYou take first steps. You see stairs. You can go upstairs or downstairs.");
+        // Decision
+        // GOING UPSTAIRS
         if (confirm("Go upstairs?")) {
-            console.log("\nYou choose to go upstairs and open the door at the top. Unfortunately this makes some noise and you see some monster charging at front of you. It is some kind of human but heavily deformed.");
-            console.log("\nWhat do you do?");
+            alert("You choose to go upstairs and open the door at the top. Unfortunately this makes some noise and you see some monster charging at front of you. It is some kind of human but heavily deformed.\n\nWhat do you do?");
+            // Decision
+            // Running downstairs
             if (confirm("Run downstairs?")) {
-                console.log("\nWhile running downstairs you fall and break your neck.");
-                youDie = console.log("\nYOU DIED!");
+                alert("While running downstairs you fall and break your neck." + youDie);
             }
+            // Decision
+            // Fighting monster
             else if (confirm("Fight this sick monster with your bare hands?")) {
-                console.log("\nYou choose to fight. FIGHTING!");
+                alert("You choose to fight. FIGHTING!");
             }
+            // Decision
+            // Doing nothing
             else {
-                console.log("\nYou choose to do nothing. The monster grabs you and eats you alive!" + youDie);
+                alert("You choose to do nothing. The monster grabs you and eats you alive!" + youDie);
             }
-
         }
+        // Decision
+        // GOING DOWNSTAIRS
         else if (confirm("Go downstairs?")) {
-            console.log("\nYou choose to go downstairs.");
+            alert("You choose to go downstairs.");
         }
+        // Decision
+        // DOING NOTHING
         else {
-            console.log("\nYou choose to do nothing. You were eaten by a monster from behind!" + youDie);
+            alert("You choose to do nothing. You were eaten by a monster from behind!" + youDie);
         }
-
-
     }
 }
 
