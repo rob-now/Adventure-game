cc0af1d3919c9ad82f6d159f599c6fa6664b62c8
rob-now <rnowakowski.prv@gmail.com>
2018-03-12 11:45:36 +0100

    If statements for health being under zero, default health object and adding comments

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
+// Default health
+const defaultHealth = {
+    character: 20,
+    monsterSmall: 15,
+    monsterBig: 22
+};
+
+// Characters statistics
 var character = {
-    health: 20,
+    health: defaultHealth.character,
     powerMin: 5,
     powerMax: 10
 };
 
 var monsterSmall = {
-    health: 15,
+    health: defaultHealth.monsterSmall,
     powerMin: 3,
     powerMax: 10
 };
 
 var monsterBig = {
-    health: 22,
+    health: defaultHealth.monsterBig,
     powerMin: 4,
     powerMax: 11
 };
 
 //var monster = {};
 
+// Printing health in HTML
 const printHealth = function () {
     document.getElementById("character-health").innerText = character.health;
     document.getElementById("monster-health").innerText = monsterSmall.health;
 };
 printHealth();
 
+// Variables declaration
 var strikeBtn = document.getElementById("strike-button");
-
 var message = document.getElementById("message");
-
 var restartButton = document.getElementById("restart-button");
 
+// Random strike function
 function strike(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Function for checking if health is 0 or lower
 function isGameOver(health) {
     return health <= 0;
 }
 
+// Game over function
 function gameOver(msg) {
     restartButton.hidden = false;
     strikeBtn.disabled = true;
     message.innerText = msg;
 }
 
-
+// Event handler for clicking strike button
 strikeBtn.onclick = function () {
     monsterSmall.health -= strike(character.powerMin, character.powerMax);
-    printHealth();
+    if (monsterSmall.health <= 0) {
+        monsterSmall.health = 0;
+    }
+    printHealth(); // Updating health value in HTML
     if (isGameOver(monsterSmall.health)) {
         gameOver("You won the game!");
         return;
@@ -55,8 +69,12 @@ strikeBtn.onclick = function () {
     strikeBtn.disabled = true;
     message.innerText = "Monster turn";
 
+    // Timeout function for monster move
     setTimeout(function () {
         character.health -= strike(monsterSmall.powerMin, monsterSmall.powerMax);
+        if (character.health <= 0) {
+            character.health = 0;
+        }
         printHealth();
         if (isGameOver(character.health)) {
             gameOver("You died!");
@@ -64,14 +82,15 @@ strikeBtn.onclick = function () {
         }
         strikeBtn.disabled = false;
         message.innerText = "";
-    }, 1000);
+    }, 500);
 };
 
 restartButton.hidden = true;
 
+// Event handler for restart button
 restartButton.onclick = function () {
-    character.health = 20;
-    monsterSmall.health = 15;
+    character.health = defaultHealth.character;
+    monsterSmall.health = defaultHealth.monsterSmall;
     printHealth();
     strikeBtn.disabled = false;
     restartButton.hidden = true;
