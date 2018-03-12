3f82ba01783dcb770068a1c4393da346242323ce
rob-now <rnowakowski.prv@gmail.com>
2018-03-12 20:15:14 +0100

    Added downstairs location and done code clean-up

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var storyDiv = document.getElementById("ready");
 var decisionDiv = document.getElementById("ready-decision");
 
 // Event (not playing game)
-var cancel = document.getElementById("cancel");
+var cancel = document.getElementById("cancel-btn");
 cancel.onclick = function () {
     document.getElementById("is-ready").className = "hidden";
     document.getElementById("not-play").className = "";
     decisionDiv.innerHTML = "";
 };
 
-var confirm = document.getElementById("yes");
+var confirm = document.getElementById("yes-btn");
 confirm.onclick = function () {
     storyDiv.innerHTML = "";
     decisionDiv.innerHTML = "";
@@ -17,26 +17,52 @@ confirm.onclick = function () {
 };
 
 function game() {
-    // STEP 01
-    var step01 = document.getElementById("step-01");
-    var step01Decision = document.getElementById("step-01-decision");
-    step01.className = "";
-    step01Decision.className = "";
-    // STEP 02
-    document.getElementById("upstairs").onclick = function () {
-        step01.innerHTML = "";
-        step01Decision.innerHTML = "";
-        var step02 = document.getElementById("step-02");
-        var step02Decision = document.getElementById("step-02-decision");
+    // Variables and constants declaration
+    const step01 = document.getElementById("step-01");
+    const step01Decision = document.getElementById("step-01-decision");
+    const step02 = document.getElementById("step-02");
+    const step02Decision = document.getElementById("step-02-decision");
+    const step03 = document.getElementById("step-03");
+    const step03Decision = document.getElementById("step-03-decision");
+
+    // Functions for showing and hiding content
+    function step01Show() {
+        step01.className = "";
+        step01Decision.className = "";
+    }
+
+    function step01Hide() {
+        step01.className = "hidden";
+        step01Decision.className = "hidden";
+    }
+
+    function step02Show() {
         step02.className = "";
         step02Decision.className = "";
+    }
+
+    function step02Hide() {
+        step02.className = "hidden";
+        step02Decision.className = "hidden";
+    }
+
+    function step03Show (){
+        step03.className = "";
+        step03Decision.className = "";
+    }
+
+    function step03Hide (){
+        step03.className = "hidden";
+        step03Decision.className = "hidden";
+    }
+    
+    // Function for story and options possible upstairs
+    function upstairs() {
+        step01Hide();
+        step03Hide();
+        step02Show();
 
         // FIGHT
-        function step02Hide() {
-            step02.className = "hidden";
-            step02Decision.className = "hidden";
-        }
-
         document.getElementById("fight-btn").onclick = function () {
             step02Hide();
             document.getElementById("fighting").className = "";
@@ -52,79 +78,24 @@ function game() {
             step02Hide();
             document.getElementById("do-nothing-div").className = "";
         };
+    }
+
+    // STEP 01 - WAKE UP
+    step01Show();
+
+    // STEP 02 - UPSTAIRS
+    document.getElementById("upstairs-btn").onclick = function () {
+        upstairs();
     };
-}
 
+    // STEP 03 - DOWNSTAIRS
+    document.getElementById("downstairs-btn").onclick = function () {
+        step01Hide();
+        step03Show();
+    };
 
-/*
-var confirmBtnHTML = "";
-var cancelBtnHTML = "";
-
-// Ready to play?
-storyDiv.innerHTML = "";
-decisionDiv.innerHTML = confirmBtnHTML + cancelBtnHTML;
-*/
-/*
-
-
-
-
-
-*/
-
-
-//document.getElementById("fighting").hidden = true;
-
-
-/*
-if () {
-    console.log("You choose not to play.");
-}
-else {
-    // Asking for age
-    var age = parseInt(prompt("What is your age?"));
-    //
-    if (!age) {
-        alert("Sorry, you cannot play. You didn't enter your age.");
-    }
-    // Too young
-    else if (age < 15) {
-        alert("Sorry, you cannot play. You are under 15 years old.");
-    }
-    // Can play
-    else {
-        // Waking up
-        alert("You wake up in some kind of cave wondering what happened. After a while you hear a huge roar coming from the dark. The time for thinking is done as you hear some movement far from your back. Now you need to run. But where is the exit...?\n\nYou take first steps. You see stairs. You can go upstairs or downstairs.");
-        // Decision
-        // GOING UPSTAIRS
-        if (confirm("Go upstairs?")) {
-            alert("You choose to go upstairs and open the door at the top. Unfortunately this makes some noise and you see some monster charging at front of you. It is some kind of human but heavily deformed.\n\nWhat do you do?");
-            // Decision
-            // Running downstairs
-            if (confirm("Run downstairs?")) {
-                alert("While running downstairs you fall and break your neck." + youDie);
-            }
-            // Decision
-            // Fighting monster
-            else if (confirm("Fight this sick monster with your bare hands?")) {
-                fighting();
-            }
-            // Decision
-            // Doing nothing
-            else {
-                alert("You choose to do nothing. The monster grabs you and eats you alive!" + youDie);
-            }
-        }
-        // Decision
-        // GOING DOWNSTAIRS
-        else if (confirm("Go downstairs?")) {
-            alert("You choose to go downstairs.");
-        }
-        // Decision
-        // DOING NOTHING
-        else {
-            alert("You choose to do nothing. You were eaten by a monster from behind!" + youDie);
-        }
+    // STEP 02 - UPSTAIRS
+    document.getElementById("upstairs-btn2").onclick = function () {
+        upstairs();
     }
 }
-*/
